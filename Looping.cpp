53717f9424dd6b3c81394f6c41c7e220a30ca8b7
julianferdiyanto <julianferdiyanto3@gmail.com>
2025-03-18 11:57:00 +0700

    penambahan pencacah turun dan perulangan while

--- Looping.cpp
+++ Looping.cpp
@@ -19,4 +19,20 @@ int main()
     cout << "Nilai n terakhir = " << n << endl;
     cout << endl;
 
+    cout << "Perulangan for pencacah turun" << endl;
+    // Perulangan dengan for
+    for (n = 5; n > 0; n--)
+    {
+        cout << "Nilai n = " << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n;
+    cout << endl;
+
+    cout << "Perulangan while" << endl;
+
+    srand(time(0));
+
+    n = rand() % 10;
+
+    cout << "Nilai aawal n = " << n << endl;
 }
