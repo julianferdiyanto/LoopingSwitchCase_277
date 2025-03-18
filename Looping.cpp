b1a4db6d7de238b33830a20d1e660a3b9fabb026
julianferdiyanto <julianferdiyanto3@gmail.com>
2025-03-18 11:47:07 +0700

    first commit

---
+++ Looping.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int n;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+
+    // Perulangan dengan for
+
+    cout << "Perulangan for pencacahan naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "Nilai n =" << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n << endl;
+    cout << endl;
+
+}
